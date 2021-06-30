f6a38d50a979511dc5f699325c8380b67980cb33
Cyril Chukwudi Chidolue <cyrilchidolue2016@gmail.com>
2021-06-30 15:27:03 +0100

    Progressive Change

--- app.js
+++ app.js
@@ -25,11 +25,90 @@ function hideMenu1() {
 var videoPlayer = document.getElementById("videoPlayer");
 var myVideo = document.getElementById("myVideo");
 
-function stopVideo(){
+function stopVideo() {
   videoPlayer.style.display = "none";
 }
 
-function startVideo(file){
+function startVideo(file) {
   myVideo.src = file;
   videoPlayer.style.display = "block";
 }
+
+// Image Gallary
+
+const gallary = document.querySelector(".gallary");
+const overlay = document.querySelector(".overlay");
+const overlayImage = overlay.querySelector(".img");
+const overlayClose = overlay.querySelector(".close");
+
+function generateHTML([h, v]) {
+  return `
+    <section class="item h${h} v${v}">
+      <img src="gallary/${randomNumber(12)}.jpg">
+        <section class="item__overlay">
+          <button>View</button>
+        </section>
+    </section>
+    `;
+}
+
+function randomNumber(limit) {
+  return Math.floor(Math.random() * limit) + 1;
+}
+
+function handleClick(e) {
+  const src = e.currentTarget.querySelector('img').src;
+  console.log(src);
+  overlayImage.src = src;
+  overlay.classList.add('open');console.log(src);
+}
+
+function close() {
+  overlay.classList.remove('open');
+}
+
+const digits = Array.from({ length: 50 }, () => [
+  randomNumber(4),
+  randomNumber(4),
+]).concat([
+  [1, 1],
+  [1, 1],
+  [1, 1],
+  [1, 1],
+  [1, 1],
+  [1, 1],
+  [1, 1],
+  [1, 1],
+  [1, 1],
+  [1, 1],
+  [1, 1],
+  [1, 1],
+  [1, 1],
+  [1, 1],
+  [1, 1],
+  [1, 1],
+  [1, 1],
+  [1, 1],
+  [1, 1],
+  [1, 1],
+  [1, 1],
+  [1, 1],
+  [1, 1],
+  [1, 1],
+  [1, 1],
+  [1, 1],
+  [1, 1],
+  [1, 1],
+  [1, 1],
+  [1, 1],
+]);
+
+const html = digits.map(generateHTML).join("");
+gallary.innerHTML = html;
+
+const items = document.querySelectorAll('.item');
+
+items.forEach(item => item.addEventListener('click',
+handleClick));
+
+overlayClose.addEventListener('click', close);
